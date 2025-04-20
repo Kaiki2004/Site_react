96d3c7d726dd9325b9fe2a47b16a12b1a63af178
Kaiki <kaikiandrade64@gmail.com>
2025-04-20 13:05:39 -0300

    FILE UPDATE AND FIXED BUGS

--- src/App.js
+++ src/App.js
@@ -6,15 +6,15 @@ import Nome from "./Componentes/Nome";
 function App() {
   const [aluno, setAluno] = useState('Marcos');
 
-  function handleChangeName(nome){
+  function handleChangeName(nome) {
     setAluno(nome);
   }
   return (
     <div className="App">
       <h1>Primeiro Site</h1>
-      <Nome aluno = {aluno}/>
-      <br/>
-      <button onClick={ () => handleChangeName('Kaiki')}>Alterar Nome</button>
+      <Nome aluno={aluno} />
+      <br />
+      <button onClick={() => handleChangeName('Kaiki')}>Alterar Nome</button>
     </div>
   );
 }
